cd34cf8bdd70786ce62d4bc63d6a9eb6dacc3ecd
Chris Mayfield <mayfiecs@jmu.edu>
2016-03-08 15:04:24 -0500

    ch03 sync w/book

--- ch03/Convert.java
+++ ch03/Convert.java
@@ -8,7 +8,8 @@ public class Convert {
     public static void main(String[] args) {
         double cm;
         int feet, inches, remainder;
-        final double centPerInch = 2.54;
+        final double CM_PER_INCH = 2.54;
+        final int IN_PER_FOOT = 12;
         Scanner in = new Scanner(System.in);
 
         // prompt the user and get the value
@@ -16,9 +17,9 @@ public class Convert {
         cm = in.nextDouble();
 
         // convert and output the result
-        inches = (int) (cm / centPerInch);
-        feet = inches / 12;
-        remainder = inches % 12;
+        inches = (int) (cm / CM_PER_INCH);
+        feet = inches / IN_PER_FOOT;
+        remainder = inches % IN_PER_FOOT;
         System.out.printf("%.2f cm = %d ft, %d in\n",
                           cm, feet, remainder);
     }
