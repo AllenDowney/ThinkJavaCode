b006db326a4470d45f9a5cdcffe727558f8b8972
Chris Mayfield <mayfiecs@jmu.edu>
2016-03-08 22:01:02 -0500

    read through ch06

---
+++ ch06/Exercise.java
@@ -0,0 +1,36 @@
+public class Exercise {
+    
+    public static void main(String[] args) {
+        boolean flag1 = isHoopy(202);
+        boolean flag2 = isFrabjuous(202);
+        System.out.println(flag1);
+        System.out.println(flag2);
+        if (flag1 && flag2) {
+            System.out.println("ping!");
+        }
+        if (flag1 || flag2) {
+            System.out.println("pong!");
+        }
+    }
+    
+    public static boolean isHoopy(int x) {
+        boolean hoopyFlag;
+        if (x % 2 == 0) {
+            hoopyFlag = true;
+        } else {
+            hoopyFlag = false;
+        }
+        return hoopyFlag;
+    }
+    
+    public static boolean isFrabjuous(int x) {
+        boolean frabjuousFlag;
+        if (x > 0) {
+            frabjuousFlag = true;
+        } else {
+            frabjuousFlag = false;
+        }
+        return frabjuousFlag;
+    }
+    
+}
