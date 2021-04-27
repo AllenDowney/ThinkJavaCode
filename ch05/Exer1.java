580a432047f9406abb5418f32c8ad9a869e28206
Renato Oliveira <renato.printf@gmail.com>
2021-04-26 23:06:04 -0300

    lessons 1,2 and 3 completed

---
+++ ch05/Exer1.java
@@ -0,0 +1,10 @@
+public class Exer1 {
+	public static void main(String[] args) {
+		int x = 11;
+			
+		if (x > 0 && x < 10)  {
+			System.out.println("positive single digit number.");
+		}
+
+	}
+}
