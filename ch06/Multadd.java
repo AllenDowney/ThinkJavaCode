e4e2ddb58e74acf4af729f93276474d3d056ae9f
Renato Oliveira <renato.printf@gmail.com>
2021-06-19 00:42:27 -0300

    corrigindo bugs

--- ch06/Multadd.java
+++ ch06/Multadd.java
@@ -4,8 +4,20 @@ public class Multadd {
         return a * b + c;
     }
 
+    public static double expSum(double x){
+        
+        multadd(a, b, c);
+    }
+
     public static void main(String[] args) {
 
+        double pi_four= Math.PI / 4;
+        double pi_cos = pi_four / 2;
+        double result = Math.sin(pi_four) + Math.cos(pi_cos);
+        double first_log = Math.log(10);
+        double second_log = Math.log(20);
+        
         System.out.println(multadd(1.0, 2.0, 3.0));
+        System.out.println(multadd(result, first_log, second_log));
     }
 }
